906a80eb3048480d622f4485dc3867e861b2e521
Angel Rodriguez <anxolin@gmail.com>
2019-01-10 18:58:30 +0100

    Use solidity 0.5.2

--- contracts/GnoDevDependencies.sol
+++ contracts/GnoDevDependencies.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.2;
 
 // NOTE:
 //  This file porpouse is just to make sure truffle compiles all of depending
@@ -11,4 +11,8 @@ pragma solidity ^0.5.0;
 import "@gnosis.pm/util-contracts/contracts/GnosisStandardToken.sol";
 import "@gnosis.pm/util-contracts/contracts/EtherToken.sol";
 
-contract GnoDevDependencies {}
+contract GnoDevDependencies {
+    string public constant symbol = "GNO";
+    string public constant name = "Gnosis";
+    uint8 public constant decimals = 18;
+}
