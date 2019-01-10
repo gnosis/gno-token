9ca82cb90aee1ad2cf6c6931cfdc9c4389efce64
Angel Rodriguez <anxolin@gmail.com>
2019-01-10 19:04:28 +0100

    Adjust gas

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 const truffleConfig = require('@gnosis.pm/util-contracts/src/util/truffleConfig')
 
 const DEFAULT_GAS_PRICE_GWEI = 5
-const GAS_LIMIT = 5e6
+const GAS_LIMIT = 6721975
 const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
 // Get the mnemonic
