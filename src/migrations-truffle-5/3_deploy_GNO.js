5dc3e5b89ac8e3af89d29ccc939adfc4ac5a71cd
Velenir <velenir@gmail.com>
2019-03-04 17:09:02 +0300

    don't link Math

--- src/migrations-truffle-5/3_deploy_GNO.js
+++ src/migrations-truffle-5/3_deploy_GNO.js
@@ -10,11 +10,6 @@ async function migrate ({
 }) {
   const BN = web3.utils.BN
   const TokenGNO = artifacts.require('TokenGNO')
-  const { Math } = _getDependencies(artifacts, network, deployer)
-
-  console.log('Link Math lib to TokenGNO')
-  await Math.deployed()
-  deployer.link(Math, TokenGNO)
 
   const owner = accounts[0]
   console.log('Deploy TokenGNO:')
@@ -25,21 +20,6 @@ async function migrate ({
     'ether'
   )
   return deployer.deploy(TokenGNO, initialTokenAmountWei)
-}
-
-function _getDependencies (artifacts, network, deployer) {
-  let Math
-  if (network === 'development') {
-    Math = artifacts.require('GnosisMath')
-  } else {
-    const contract = require('truffle-contract')
-    Math = contract(require('@gnosis.pm/util-contracts/build/contracts/Math'))
-    Math.setProvider(deployer.provider)
-  }
-
-  return {
-    Math
-  }
 }
 
 module.exports = migrate
